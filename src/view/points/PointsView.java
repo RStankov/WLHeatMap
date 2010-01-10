1349b99bb4addaf6207b5bc5bbf12f63d38c5ad1
RStankov <rstankov@gmail.com>
2010-01-10 21:20:26 +0200

    add automatic horizontal resize to map and grid in points view

--- src/view/points/PointsView.java
+++ src/view/points/PointsView.java
@@ -47,33 +47,24 @@ public class PointsView extends JPanel {
 		
 		GroupLayout layout = new GroupLayout(this);
 		
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(map, GroupLayout.Alignment.LEADING)
-                    .addComponent(panel, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 505, Short.MAX_VALUE))
-                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(map, GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE)
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panel, GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE)
-                .addContainerGap())
-        );
+		layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+			.addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+			.addContainerGap().addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+				.addComponent(map, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 467, Short.MAX_VALUE)
+				.addComponent(panel, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 467, Short.MAX_VALUE)
+			)
+			.addContainerGap())
+		);
+		layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+			.addGroup(layout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(map, GroupLayout.DEFAULT_SIZE, 354, Short.MAX_VALUE)
+					.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE)
+					.addContainerGap()
+			)
+		);
 
 		setLayout(layout);
-        
-        setLayout(layout);
-        /*
-	    setLayout(new GridLayout(2,1));
-	    
-		add(map);
-		add(panel);
-		*/
 	}
 }
